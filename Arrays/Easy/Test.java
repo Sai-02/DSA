a8367e8291f755f0008fcf00b0ecd95611b977c3
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-07-05 21:57:09 +0530

    test

--- Arrays/Easy/Test.java
+++ Arrays/Easy/Test.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.util.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
- public class Solution {
+ public class Test {
 
   public static void main(String[] args) throws java.lang.Exception {
     FastReader scan = new FastReader();
