87d715994fe94c6e86615c7a6ee680cbdb815874
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-07-05 21:50:25 +0530

    Create Solution.java

---
+++ Arrays/Easy/Solution.java
@@ -0,0 +1,61 @@
+/* package codechef; // don't place package name! */
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+ public class Solution {
+
+  public static void main(String[] args) throws java.lang.Exception {
+    int n=scan.nextInt();
+    System.out.println(n);
+    String s=scan.nextLine();
+    System.out.println(s);
+    
+    
+
+  }
+
+  static class FastReader {
+    BufferedReader br;
+    StringTokenizer st;
+
+    public FastReader() {
+      br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    String next() {
+      while (st == null || !st.hasMoreElements()) {
+        try {
+          st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+
+    long nextLong() {
+      return Long.parseLong(next());
+    }
+
+    double nextDouble() {
+      return Double.parseDouble(next());
+    }
+
+    String nextLine() {
+      String str = "";
+      try {
+        str = br.readLine();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return str;
+    }
+  }
+}
