4aa0d6bd15645a769448c55581ec2bb113f2ef1c
Sai Prashant Saxena <56087847+Sai-02@users.noreply.github.com>
2020-07-05 21:51:15 +0530

    Update Solution.java

--- Arrays/Easy/Solution.java
+++ Arrays/Easy/Solution.java
@@ -8,6 +8,7 @@ import java.util.*;
  public class Solution {
 
   public static void main(String[] args) throws java.lang.Exception {
+    FastReader scan = new FastReader();
     int n=scan.nextInt();
     System.out.println(n);
     String s=scan.nextLine();
