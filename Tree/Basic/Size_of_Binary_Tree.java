0f40182fd31e6c162fe3e4d55939379b4b5116db
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2020-10-20 13:20:49 +0530

    Size of Binary Tree

---
+++ Tree/Basic/Size_of_Binary_Tree.java
@@ -0,0 +1,168 @@
+
+/*
+Given a binary tree of size N, you have to count number of nodes in it. For example, count of nodes in below tree is 4.
+
+        1
+     /      \
+   10      39
+  /
+5
+
+Input:
+First line of input contains the number of test cases T. For each test case, there will be only a single line of input which is a string representing the tree as described below: 
+
+The values in the string are in the order of level order traversal of the tree where, numbers denote node values, and a character “N” denotes NULL child.
+
+For example:
+
+For the above tree, the string will be: 1 2 3 N N 4 6 N 5 N N 7 N
+
+Output:
+For each testcase in new line, print the number of nodes.
+
+User Task:
+Since this is a functional problem you don't have to worry about input, you just have to complete the function getSize().
+
+Constraints:
+1 <= T <= 30
+1 <= N <= 104
+Example:
+Input:
+2
+1 2 3
+10 5 9 N 1 3 6 
+
+Output:
+3
+6
+
+Explanation:
+Testcase 2: Given Tree is :
+                                10
+                             /        \
+                           5          9
+                             \       /     \
+                              1    3       6
+There are six nodes in the tree . 
+*/
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Size_of_Binary_Tree {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    static void printInorder(Node root) {
+        if (root == null)
+            return;
+
+        printInorder(root.left);
+        System.out.print(root.data + " ");
+
+        printInorder(root.right);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+
+            Tree g = new Tree();
+            System.out.println(g.getSize(root));
+            t--;
+
+        }
+    }
+}
+
+// } Driver Code Ends
+
+/* Complete The Function Below */
+class Tree {
+    public static int getSize(Node root) {
+        if (root == null) {
+            return 0;
+
+        }
+
+        return 1 + getSize(root.left) + getSize(root.right);
+
+    }
+}
