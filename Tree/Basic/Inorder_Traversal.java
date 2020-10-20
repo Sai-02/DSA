9908da3e947aa23a063e18dc897a8009d818f651
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2020-10-20 16:10:09 +0530

    Inorder Traversal
    Inorder Traversal in  tree

---
+++ Tree/Basic/Inorder_Traversal.java
@@ -0,0 +1,159 @@
+
+/*
+Given a Binary Tree, find the In-Order Traversal of it.
+
+Example 1:
+
+Input:
+       1
+     /  \
+    3    2
+Output: 3 1 2
+
+Example 2:
+
+Input:
+        10
+     /      \ 
+    20       30 
+  /    \    /
+ 40    60  50
+Output: 40 20 60 10 50 30
+
+Your Task:
+You don't need to read input or print anything.
+ Your task is to complete the function inOrder() 
+ that takes root node of the tree as input and returns a list 
+ containing the In-Order Traversal of the given Binary Tree.
+
+Expected Time Complexity: O(N).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 <= Number of nodes <= 105
+1 <= Data of a node <= 105
+
+*/
+
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+public class Inorder_Traversal {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length)
+                break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree2 g = new Tree2();
+            ArrayList<Integer> res = g.inOrder(root);
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.print("\n");
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+// User function Template for Java
+
+/*
+ * A Binary Tree node
+ * 
+ * class Node { int data; Node left, right; Node(int item) { data = item; left =
+ * right = null; } }
+ */
+class Tree2 {
+    // Return a list containing the inorder traversal of the given tree
+    ArrayList<Integer> inOrder(Node root) {
+
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        if (root == null) {
+            return a;
+        }
+        a.addAll(inOrder(root.left));
+        a.add(root.data);
+        a.addAll(inOrder(root.right));
+        return a;
+
+    }
+
+}
