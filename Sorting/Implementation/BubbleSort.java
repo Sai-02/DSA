987a98f87ffc79b38586419654f69ab6fad64d13
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2021-06-17 21:40:53 +0530

    Bubble Sort optimization

--- Sorting/Implementation/BubbleSort.java
+++ Sorting/Implementation/BubbleSort.java
@@ -19,12 +19,17 @@ public class BubbleSort {
 
     public static void getBubbleSort(int a[], int n) {
         for (int i = 0; i < n - 1; i++) {
+            boolean swaped = false;
             for (int j = 0; j < n - i - 1; j++) {
                 if (a[j] > a[j + 1]) {
                     int temp = a[j];
                     a[j] = a[j + 1];
                     a[j + 1] = temp;
+                    swaped = true;
                 }
+            }
+            if (!swaped) {
+                return;
             }
         }
 
