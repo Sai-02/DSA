06bb1254648c5a6f4997c28567a9324a865cf850
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-02-08 15:51:23 +0530

    814. Binary Tree Pruning

---
+++ Tree/Medium/814_Binary_Tree_Pruning.java
@@ -0,0 +1,70 @@
+/**
+ * Given the root of a binary tree, return the same tree where every subtree (of
+ * the given tree) not containing a 1 has been removed.
+ * 
+ * A subtree of a node node is node plus every node that is a descendant of
+ * node.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,0,0,1]
+ * Output: [1,null,0,null,1]
+ * Explanation:
+ * Only the red nodes satisfy the property "every subtree not containing a 1".
+ * The diagram on the right represents the answer.
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,0,1,0,0,0,1]
+ * Output: [1,null,1,null,1]
+ * Example 3:
+ * 
+ * 
+ * Input: root = [1,1,0,1,1,0,1,0]
+ * Output: [1,1,0,1,1,null,1]
+ * 
+ * 
+ * Constraints:
+ * 
+ * The number of nodes in the tree is in the range [1, 200].
+ * Node.val is either 0 or 1.
+ * Accepted
+ * 134,292
+ * Submissions
+ * 188,987
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode pruneTree(TreeNode root) {
+        if (root == null)
+            return null;
+        TreeNode left = pruneTree(root.left);
+        TreeNode right = pruneTree(root.right);
+        if (left == null && right == null && root.val != 1)
+            return null;
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+}
