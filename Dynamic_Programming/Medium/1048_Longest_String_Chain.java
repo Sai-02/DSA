a6c7088f6ebe643ca80b26e0819d70404ad5f414
Sai Prashant Saxena <saiprashant.saxena@gmail.com>
2022-06-15 12:14:23 +0530

    1048. Longest String Chain

---
+++ Dynamic_Programming/Medium/1048_Longest_String_Chain.java
@@ -0,0 +1,88 @@
+
+/**
+ * You are given an array of words where each word consists of lowercase English
+ * letters.
+ * 
+ * wordA is a predecessor of wordB if and only if we can insert exactly one
+ * letter anywhere in wordA without changing the order of the other characters
+ * to make it equal to wordB.
+ * 
+ * For example, "abc" is a predecessor of "abac", while "cba" is not a
+ * predecessor of "bcad".
+ * A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1,
+ * where word1 is a predecessor of word2, word2 is a predecessor of word3, and
+ * so on. A single word is trivially a word chain with k == 1.
+ * 
+ * Return the length of the longest possible word chain with words chosen from
+ * the given list of words.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: words = ["a","b","ba","bca","bda","bdca"]
+ * Output: 4
+ * Explanation: One of the longest word chains is ["a","ba","bda","bdca"].
+ * Example 2:
+ * 
+ * Input: words = ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+ * Output: 5
+ * Explanation: All the words can be put in a word chain ["xb", "xbc", "cxbc",
+ * "pcxbc", "pcxbcf"].
+ * Example 3:
+ * 
+ * Input: words = ["abcd","dbqca"]
+ * Output: 1
+ * Explanation: The trivial word chain ["abcd"] is one of the longest word
+ * chains.
+ * ["abcd","dbqca"] is not a valid word chain because the ordering of the
+ * letters is changed.
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= words.length <= 1000
+ * 1 <= words[i].length <= 16
+ * words[i] only consists of lowercase English letters.
+ */
+
+import java.util.*;
+
+class Solution {
+    public int longestStrChain(String[] words) {
+        Arrays.sort(words, (a1, a2) -> a1.length() - a2.length());
+        int dp[] = new int[words.length];
+        int maxLength = 0;
+        for (int i = 0; i < words.length; i++) {
+            String wordA = words[i];
+            int count = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                String wordB = words[j];
+                if (wordA.length() - wordB.length() != 1)
+                    continue;
+                boolean inserted = false, error = false;
+                int indexA = 0, indexB = 0;
+                while (indexA < wordA.length() && indexB < wordB.length()) {
+                    if (wordA.charAt(indexA) == wordB.charAt(indexB)) {
+                        indexA++;
+                        indexB++;
+                    } else {
+                        if (!inserted) {
+                            inserted = true;
+                            indexA++;
+                        } else {
+                            error = true;
+                            break;
+                        }
+                    }
+                }
+                if (!error) {
+                    count = Math.max(count, dp[j]);
+                }
+            }
+            dp[i] = count + 1;
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        return maxLength;
+    }
+}
